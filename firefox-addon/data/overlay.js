472a2ea4ff9d938dd6a149ac0614683b68d8b5fa
juni daye <junidaye@localhost>
2013-04-06 17:17:25 +0200

    Format sources with Aptana.

--- firefox-addon/data/overlay.js
+++ firefox-addon/data/overlay.js
@@ -1,5 +1,5 @@
 function Overlay(panelHtml, panelCss) {
-	
+
 	this.visible = false;
 
 	this.isVisible = function() {
@@ -8,18 +8,13 @@ function Overlay(panelHtml, panelCss) {
 
 	this.show = function() {
 		this.visible = true;
-var overlayDiv = jQuery('<div id="overlay" '
-	   	//+ ' style="padding-left: 1em; border: 2px solid black; position: fixed; top: 0; left: 10%; width: 80%; height: 170px; background-color: #FFF; filter:alpha(opacity=95); -moz-opacity:0.95; -khtml-opacity: 0.95; opacity: 0.95; z-index: 10000;'
-		+ ' class="ftc_overlayDiv"'
-	   	+ '">'
-		+ panelHtml
-		+ '	</div>');
-overlayDiv.draggable();
-overlayDiv.resizable();
-overlayDiv.appendTo(document.body);
+		var overlayDiv = jQuery('<div id="overlay" class="ftc_overlayDiv">' + panelHtml + '	</div>');
+		overlayDiv.draggable();
+		overlayDiv.resizable();
+		overlayDiv.appendTo(document.body);
 
-var cssElement = jQuery('<style type="text/css">' + panelCss + '</style>');
-cssElement.appendTo(document.head);
+		var cssElement = jQuery('<style type="text/css">' + panelCss + '</style>');
+		cssElement.appendTo(document.head);
 	}
 
 	this.hide = function() {
--- firefox-addon/data/page.js
+++ firefox-addon/data/page.js
@@ -1,5 +1,5 @@
 self.port.on("init", function(panelHtml, panelCss) {
-//	document.body.style.border = "5px solid red";
+	// document.body.style.border = "5px solid red";
 
 	var overlay = new Overlay(panelHtml, panelCss);
 	overlay.show();
