ca5c4a61e1f616fda21d3669a923a86c069ab400
juni daye <junidaye@localhost>
2013-04-07 17:15:15 +0200

    Improve looks a bit.

--- firefox-addon/data/overlay-integration.js
+++ firefox-addon/data/overlay-integration.js
@@ -9,8 +9,8 @@ function OverlayIntegration(pageCss, panelHtml, panelCss) {
 	this.show = function() {
 		// write overlay HTML to document
 		this.visible = true;
-		var overlayDiv = jQuery('<div id="overlay" class="ftc_overlayDiv"></div>');
-		overlayDiv.draggable();
+		var overlayDiv = jQuery('<div class="ftc_overlayDiv"></div>');
+		overlayDiv.draggable({iframeFix: true, cursor: "crosshair"});
 		overlayDiv.resizable();
 		overlayDiv.appendTo(document.body);
 
@@ -19,7 +19,7 @@ function OverlayIntegration(pageCss, panelHtml, panelCss) {
 		cssElement.appendTo(document.head);
 		
 		// create iframe
-		var iframe = jQuery('<iframe style="width:100%; height:270px;">');
+		var iframe = jQuery('<iframe frameborder="0" style="width:100%; height:270px;">');
 		iframe.appendTo(overlayDiv);
 		iframe.load(function(){
 			
@@ -32,6 +32,9 @@ function OverlayIntegration(pageCss, panelHtml, panelCss) {
 		
 			var overlayContent = new OverlayContent(panelDocument);
 			overlayContent.init();
+			
+			var draggableHandler = jQuery('<div style="position: absolute; height: 30px; width: 30px; background-color: black; z-index: 1000;"></div>');
+			draggableHandler.appendTo(overlayDiv);
 		});
 	}
 
