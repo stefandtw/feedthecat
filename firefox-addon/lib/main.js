f69d3f9f65dbe9f8c1216a7a462678945a55c9db
juni daye <junidaye@localhost>
2013-04-16 18:44:17 +0200

    Add an icon.

--- firefox-addon/lib/main.js
+++ firefox-addon/lib/main.js
@@ -20,7 +20,7 @@ var dialogCss = data.load("dialog.css")//
 var widget = widgets.Widget({
 	id : "feedthecat-widget",
 	label : "feedthecat",
-	contentURL : "http://www.mozilla.org/favicon.ico",
+	contentURL : data.url("images/Cat_-3.svg"),
 	onClick : function() {
 		var worker = tabs.activeTab.attach({
 			contentScriptFile : [data.url("helper-functions.js"), data.url("page.js"), data.url("page-mantle.js"), data.url("selector.js"), data.url("highlighter.js"), data.url("dialog-integration.js"), data.url("dialog-content.js"), data.url("jquery/jquery-1.9.1.min.js"), data.url("jquery-ui-1.10.2.custom/js/jquery-ui-1.10.2.custom.min.js"), data.url("jquery/jquery-migrate-1.1.1.min.js"), data.url("HTML-KickStart-master/js/kickstart.js")]
